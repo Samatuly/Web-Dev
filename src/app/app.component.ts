6ac1dcc5372722bda069f0ed09225526775a85e9
Serikbolsyn <serikbolsyn.200904@gmail.com>
2023-04-14 14:23:36 +0600

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Company} from "./models";
+import {CompaniesService} from "./companies.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hh-front';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import { CompaniesComponent } from './companies/companies.component';
+import { CompanyVacanciesComponent } from './company-vacancies/company-vacancies.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompaniesComponent,
+    CompanyVacanciesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterOutlet,
+    AppRoutingModule,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
